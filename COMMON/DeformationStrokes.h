ab2c46f64578083b1a24779c4b4cf65b44419f84
Takashi IJIRI <takashi.ijiri80@gmail.com>
2026-01-29 19:12:14 +0900

    add tcolor.h

--- COMMON/DeformationStrokes.h
+++ COMMON/DeformationStrokes.h
@@ -4,6 +4,7 @@
 #include <vector>
 #include <set>
 #include "tmath.h"
+#include "tcolor.h"
 
 class PlanarCurve
 {
---
+++ COMMON/tcolor.h
@@ -0,0 +1,27 @@
+#ifndef COMMON_TCOLOR_H_
+#define COMMON_TCOLOR_H_
+
+#pragma unmanaged
+
+//red green blue
+static float COLOR_R[4] = { 1.0f, 0.0f, 0.0f, 0.5f };
+static float COLOR_RH[4] = { 0.5f, 0.0f, 0.0f, 0.5f };
+static float COLOR_G[4] = { 0.0f, 1.0f, 0.0f, 0.5f };
+static float COLOR_GH[4] = { 0.0f, 0.5f, 0.0f, 0.5f };
+static float COLOR_B[4] = { 0.0f, 0.0f, 1.0f, 0.5f };
+static float COLOR_BH[4] = { 0.0f, 0.0f, 0.5f, 0.5f };
+
+//yellow , magenda, cyan
+static float COLOR_Y[4] = { 1.0f, 1.0f, 0.0f, 0.5f };
+static float COLOR_YH[4] = { 0.5f, 0.5f, 0.0f, 0.5f };
+static float COLOR_M[4] = { 1.0f, 0.0f, 1.0f, 0.5f };
+static float COLOR_MH[4] = { 0.5f, 0.0f, 0.5f, 0.5f };
+static float COLOR_C[4] = { 0.0f, 1.0f, 1.0f, 0.5f };
+static float COLOR_CH[4] = { 0.0f, 0.5f, 0.5f, 0.5f };
+
+static float COLOR_W[4] = { 1.0f, 1.0f, 1.0f, 0.5f };
+static float COLOR_GRAY[4] = { 0.5f, 0.5f, 0.5f, 0.5f };
+
+static float COLOR_SHIN64[1] = { 64 };
+
+#endif
